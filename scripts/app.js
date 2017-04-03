902322b6d9bac29362c8b0e66d8151eed76f9471
Giulia Scalaberni <scalaberni.giulia@gmail.com>
2017-04-03 15:58:54 +0200

    added some js

--- scripts/app.js
+++ scripts/app.js
@@ -8,14 +8,65 @@
  *
  * Main module of the application.
  */
-angular
+ var documentsApp = angular.module('documentsApp', [
+   'ngAnimate',
+   'ngCookies',
+   'ngResource',
+   'ngRoute',
+   'ngSanitize',
+   'ngTouch',
+   'cloudinary',
+   'photoAlbumAnimations',
+   'photoAlbumControllers',
+   'photoAlbumServices'
+ ]);
+
+ documentsApp.config(['$routeProvider',
+   function ($routeProvider) {
+     $routeProvider
+
+
+     .when('/photos', {
+       templateUrl: 'views/photo-list.html',
+       resolve: {
+         photoList: function ($q, $rootScope, album) {
+           if (!$rootScope.serviceCalled) {
+             return album.photos({}, function (v) {
+               $rootScope.serviceCalled = true;
+               $rootScope.photos = v.resources;
+             });
+           } else {
+             return $q.when(true);
+           }
+         }
+       }
+     }).when('/', {
+       templateUrl: 'views/main.html',
+       controller: 'MainCtrl',
+       controllerAs: 'main'
+     }).when('/about', {
+       templateUrl: 'views/about.html',
+       controller: 'AboutCtrl',
+       controllerAs: 'about'
+     })
+     .otherwise({
+       redirectTo: '/'
+     });
+   }]);
+
+
+/*angular
   .module('documentsApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'cloudinary',
+    'photoAlbumAnimations',
+    'photoAlbumControllers',
+    'photoAlbumServices'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -32,4 +83,4 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  });
+  });*/
---
+++ scripts/config.js
@@ -0,0 +1,5 @@
+documentsApp.config(['cloudinaryProvider', function (cloudinaryProvider) {
+  cloudinaryProvider
+      .set("cloud_name", "giulia")
+      .set("upload_preset", "giulia_unsigned");
+}]);

8dfa9600ebaee57d4feef277adc6f304fbfef25c
Giulia Scalaberni <scalaberni.giulia@gmail.com>
2017-05-04 20:01:33 +0200

    ADD PHOTO WORKS

--- scripts/controllers/cogn_add_face.js
+++ scripts/controllers/cogn_add_face.js
@@ -13,17 +13,19 @@ cognAddFileController.controller('cognAddFileCtrl', function($scope,$rootScope, 
   alert($rootScope.url);
   var params = {
       // Request parameters
-      "returnFaceId": "true",
-      "returnFaceLandmarks": "false",
-      "returnFaceAttributes": "glasses",
+      "personGroupId": "050498",
+      "personId": "bf0d6b4a-c928-487e-91cb-efab9abf0435",
+      "userData": "photoXXX",
   };
+//westus.api.cognitive.microsoft.com/face/v1.0/persongroups/{personGroupId}/persons/{personId}/persistedFaces[?userData][&targetFace]
+//"https://westus.api.cognitive.microsoft.com/face/v1.0/detect?" + $.param(params)
   var obj='{"url":"'+$rootScope.url+'"}';
   $http({
         method : "POST",
-        url : "https://westus.api.cognitive.microsoft.com/face/v1.0/detect?" + $.param(params),
+        url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/{personGroupId}/persons/{personId}/persistedFaces?" + $.param(params),
         headers: {
           'Content-Type': 'application/json',
-          'Ocp-Apim-Subscription-Key':'66dfd0519d9144d798e72d8fb4ce03e1'
+          'Ocp-Apim-Subscription-Key':'19ea017349b84f56aa12bf38a4b50756'
         },
          data: obj
     }).then(function mySucces(response) {

82d45f6ec7405f9c0d85b17c2fbcf67aabf3ab78
Giulia Scalaberni <scalaberni.giulia@gmail.com>
2017-05-04 22:51:56 +0200

    ok get faces and add

--- scripts/controllers/cogn_add_face.js
+++ scripts/controllers/cogn_add_face.js
@@ -33,24 +33,28 @@ cognAddFileController.controller('cognAddFileCtrl', function($scope,$rootScope, 
         alert($scope.myWelcome = response.data.error.code+": "+response.data.error.message);
     });
 
-/*  $.ajax({
-      url: "https://westus.api.cognitive.microsoft.com/face/v1.0/detect?" + $.param(params),
-      beforeSend: function(xhrObj){
-          // Request headers
-          //xhrObj.setRequestHeader("Content-Type","application/octet-stream");
-          xhrObj.setRequestHeader("Content-Type","application/json");
-          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","66dfd0519d9144d798e72d8fb4ce03e1");
-      },
-      type: "POST",
-      // Request body
-      data:    obj
-  })
-  .done(function(data) {
-      alert("success");
-  })
-  .fail(function() {
-      alert("error");
-  });
-});*/
+    $scope.getFaces=function(){
+      var params = {
+          // Request parameters
+          "personGroupId": "050498",
+          "personId": "bf0d6b4a-c928-487e-91cb-efab9abf0435"
+      };
+      $http({
+            method : "GET",
+            url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/{personGroupId}/persons/{personId}?" + $.param(params),
+            headers: {
+              'Content-Type': 'application/json',
+              'Ocp-Apim-Subscription-Key':'19ea017349b84f56aa12bf38a4b50756'
+            },
+            // data: obj
+        }).then(function mySucces(response) {
+          $scope.faces = response.data;
+            alert($scope.faces);
+        }, function myError(response) {
+            alert("ERRORE");
+        });
 
+
+
+    };
 });

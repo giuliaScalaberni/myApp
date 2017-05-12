fa2bdb7bc4a5843efd8c673ecaa88ec529fc175b
Giulia Scalaberni <scalaberni.giulia@gmail.com>
2017-05-13 00:21:25 +0200

    WIP grouplist -> person list

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -9,9 +9,7 @@
  */
 angular.module('documentsApp')
   .controller('MainCtrl', function ($scope, $http) {
-    var params = {
-        // Request parameters
-    };
+
     $http({
           method : "GET",
           url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups",
@@ -22,10 +20,32 @@ angular.module('documentsApp')
           // data: obj
       }).then(function mySucces(response) {
         $scope.groups = response.data;
+
       }, function myError(response) {
           alert("ERRORE");
       });
 
 
+      $scope.findPeople=function(){
+        var $this = $(this);
+        alert($this);
+        var params={
+        "personGroupId": $this
+      };
+        $http({
+              method : "GET",
+              url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/{personGroupId}?"+$.param(params),
+              headers: {
+                'Content-Type': 'application/json',
+                'Ocp-Apim-Subscription-Key':'19ea017349b84f56aa12bf38a4b50756'
+              },
+          }).then(function mySucces(response) {
+            $scope.people = response.data;
+
+          }, function myError(response) {
+              alert("ERRORE");
+          });
+
+      };
 
      });

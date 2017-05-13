903dbd9defdda9f9d27952e740019a086198576b
Giulia Scalaberni <scalaberni.giulia@gmail.com>
2017-05-13 16:17:19 +0200

    bit layout changes

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -33,6 +33,7 @@ angular.module('documentsApp')
                 'Ocp-Apim-Subscription-Key':'19ea017349b84f56aa12bf38a4b50756'
               },
           }).then(function mySucces(response) {
+            
             if (response.data.length==0){
               $scope.people='';
               $scope.warningAlert=1;

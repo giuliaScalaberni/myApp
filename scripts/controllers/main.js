6f42546af914d1ba47fbae3398bd1a61c53ddf2d
Giulia Scalaberni <scalaberni.giulia@gmail.com>
2017-05-13 16:06:03 +0200

    bootstrap alert whn no people

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 angular.module('documentsApp')
   .controller('MainCtrl', function ($scope, $http) {
-
+    $scope.warningAlert = 0;
     $http({
           method : "GET",
           url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups",
@@ -25,19 +25,25 @@ angular.module('documentsApp')
       });
 
       $scope.findPeople=function(id){
-        alert(id);
-        var params={
-        "personGroupId": id
-      };
         $http({
               method : "GET",
-              url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/{personGroupId}?"+$.param(params),
+              url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/"+id+"/persons",
               headers: {
                 'Content-Type': 'application/json',
                 'Ocp-Apim-Subscription-Key':'19ea017349b84f56aa12bf38a4b50756'
               },
           }).then(function mySucces(response) {
+            if (response.data.length==0){
+              $scope.people='';
+              $scope.warningAlert=1;
+              //alert("Warning: no user for this group");
+            }
+            else{
+              $scope.warningAlert=0;
+                $scope.people='';
+            //alert(response.data[0].name)
             $scope.people = response.data;
+            }
 
           }, function myError(response) {
               alert("ERRORE");

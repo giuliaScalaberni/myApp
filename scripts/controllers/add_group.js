67d8730e415a54e0e064958873b97d04177e3ae1
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-05-22 16:02:49 +0200

    a bit layout changes

--- scripts/controllers/add_group.js
+++ scripts/controllers/add_group.js
@@ -7,35 +7,25 @@
  * # AboutCtrl
  * Controller of the documentsApp
  */
-
-    var addGroupController = angular.module('addGroupController', []);
+var addGroupController = angular.module('addGroupController', []);
 addGroupController.controller('addGroupCtrl', function($scope,$rootScope, $http,$location) {
-
   $scope.createGroup=function(){
     var obj='{"name":"'+$('#name').val()+'", "userData":"'+$('#data').val()+'"}';
     $http({
-          method : "PUT",
-          url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/"+$('#id').val(),
-          headers: {
-            'Content-Type': 'application/json',
-            'Ocp-Apim-Subscription-Key':'19ea017349b84f56aa12bf38a4b50756'
+            method : "PUT",
+            url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/"+$('#id').val(),
+            headers: {
+              'Content-Type': 'application/json',
+              'Ocp-Apim-Subscription-Key':'19ea017349b84f56aa12bf38a4b50756'
+            },
+            data: obj
+          })
+          .then(function mySucces(response) {
+            alert("New group created!");
+            $location.path('/index');
           },
-          data: obj
-      }).then(function mySucces(response) {
-
-      alert("ok");
-
-      }, function myError(response) {
-          alert("ERRORE");
-      });
-
-  };
-
-
-
-
-
-
-
-
+          function myError(response) {
+            alert(response.data.error.code+": "+response.data.error.message);
+          });
+        };
 });

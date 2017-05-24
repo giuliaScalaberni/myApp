aea60e13721bc019a6de6d38e00c6bd54e4a6c91
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-05-24 17:59:14 +0200

    ok delete

--- scripts/controllers/get_person.js
+++ scripts/controllers/get_person.js
@@ -9,7 +9,7 @@
  */
 
     var getPersonController = angular.module('getPersonController', []);
-getPersonController.controller('getPersonCtrl', function($scope,$rootScope, $http) {
+getPersonController.controller('getPersonCtrl', function($scope,$rootScope, $http, $route, $location) {
 
     var params = {
         // Request parameters
@@ -34,9 +34,58 @@ getPersonController.controller('getPersonCtrl', function($scope,$rootScope, $htt
         $scope.faces = response.data;
 
       }, function myError(response) {
-          alert("No parameters to get a response");
+          //alert("No parameters to get a response");
+          $location.path("/");
       });
 
+      $scope.trashPhoto=function(pid){
+          $scope.id=pid;
+          $scope.item="photo";
+          $('#modalDelete').modal('show');
 
+        };
+        $scope.trashPerson=function(){
+          $scope.id=$rootScope.userId;
+          $scope.item="person";
+          $('#modalDelete').modal('show');
+
+        }
+
+        $scope.delete=function(){
+           $('#modalDelete').modal('hide');
+           if ($scope.item=="photo"){
+               $http({
+                     method : "delete",
+                     url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/"+$rootScope.groupId+"/persons/"+$rootScope.userId+"/persistedFaces/"+$scope.id,
+                     headers: {
+                       'Content-Type': 'application/json',
+                       'Ocp-Apim-Subscription-Key':'19ea017349b84f56aa12bf38a4b50756'
+                     }
+                 }).then(function mySucces(response) {
+                    alert("Delete succeeded");
+                    $scope.id="";
+                    $route.reload();
+                 }, function myError(response) {
+                     alert("Not possible to delete");
+                 });
+          }
+          else
+          {
+            $http({
+                  method : "delete",
+                  url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/"+$rootScope.groupId+"/persons/"+$rootScope.userId,
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'Ocp-Apim-Subscription-Key':'19ea017349b84f56aa12bf38a4b50756'
+                  }
+              }).then(function mySucces(response) {
+                 alert("Delete succeeded");
+                   $location.path("/");
+
+              }, function myError(response) {
+                  alert("Not possible to delete");
+              });
+          }
+        };
 
   });
--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -63,14 +63,8 @@ angular.module('documentsApp')
       $('#modalDelete').modal('show');
       $scope.item="group";
     };
-    $scope.trashP=function(pid){
-        $scope.pid=pid;
-        $('#modalDelete').modal('show');
-        $scope.item="person";
-      };
     $scope.delete=function(){
        $('#modalDelete').modal('hide');
-       if ($scope.item=="group"){
        $http({
              method : "delete",
              url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/"+$scope.gid,
@@ -86,24 +80,6 @@ angular.module('documentsApp')
          }, function myError(response) {
              alert("Not possible to delete");
          });
-         }
-         else {
-           $http({
-                 method : "delete",
-                 url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/"+$rootScope.groupId+"/persons/"+$scope.pid,
-                 headers: {
-                   'Content-Type': 'application/json',
-                   'Ocp-Apim-Subscription-Key':'19ea017349b84f56aa12bf38a4b50756'
-                 }
-             }).then(function mySucces(response) {
-                alert("Delete succeeded");
-                $scope.gid="";
-                $scope.pid="";
-                $route.reload();
-             }, function myError(response) {
-                 alert("Not possible to delete");
-             });
-         }
 
     };
 });

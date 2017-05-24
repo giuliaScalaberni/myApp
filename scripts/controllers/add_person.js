7d5dedb3ca2cb56ab58b1db4449d76cc58229cda
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-05-24 18:55:24 +0200

    wip add person

--- scripts/controllers/add_person.js
+++ scripts/controllers/add_person.js
@@ -9,11 +9,14 @@
  */
 var addPersonController = angular.module('addPersonController', []);
 addPersonController.controller('addPersonCtrl', function($scope,$rootScope, $http,$location) {
+  if ($rootScope.groupId==undefined){
+    $location.path('/');
+  }
   $scope.createPerson=function(){
     var obj='{"name":"'+$('#name').val()+'", "userData":"'+$('#data').val()+'"}';
     $http({
             method : "PUT",
-            url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/"+$('#id').val(),
+            url : "https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/"+$rootScope.groupId+"/persons",
             headers: {
               'Content-Type': 'application/json',
               'Ocp-Apim-Subscription-Key':'19ea017349b84f56aa12bf38a4b50756'
@@ -21,11 +24,13 @@ addPersonController.controller('addPersonCtrl', function($scope,$rootScope, $htt
             data: obj
           })
           .then(function mySucces(response) {
-            alert("New group created!");
-            $location.path('/index');
+            alert("New person created! "+ response.personId);
+            $rootScope.userId=response.personId;
+            $location.path('/get-person');
           },
           function myError(response) {
             alert(response.data.error.code+": "+response.data.error.message);
+            
           });
         };
 });

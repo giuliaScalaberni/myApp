ea54f23e4549ef486be5ced846f04cc292f38079
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-06-06 19:02:15 +0200

    resolved i70

--- scripts/controllers/welcome.js
+++ scripts/controllers/welcome.js
@@ -1,6 +1,8 @@
 'use strict';
 var welcomeController = angular.module('welcomeController', []);
 welcomeController.controller('WelcomeCtrl', function($scope,$rootScope, $http,$location) {
-
+if ($rootScope.action == undefined){
+  $location.path("/");
+}
 
 });

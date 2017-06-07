2fa143322a48e5849861d44439d31068eaeb44fd
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-06-07 18:26:36 +0200

    bug fixed in message alert delete snap

--- scripts/controllers/get_person.js
+++ scripts/controllers/get_person.js
@@ -99,6 +99,17 @@ getPersonController.controller('getPersonCtrl', function($scope,$rootScope, $htt
                      }
                  }).then(function mySucces(response) {
                     alert("Delete succeeded");
+                    $http({
+                      method : "POST",
+                      url : 'http://localhost:80/deleteFace.php',
+                      data: { faceId: $scope.id },
+                      headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+
+                    }).then(function mySucces() {
+                        $location.path("/");
+                      }), function myError(r) {
+                          alert("Error: "+r);
+                      };
                     $scope.id="";
                     $route.reload();
                  }, function myError(response) {

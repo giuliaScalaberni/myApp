c6bff2bed78470dc9a971680c612ee99c1b6a303
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-06-08 23:14:36 +0200

    ok data in table

--- scripts/controllers/presences.js
+++ scripts/controllers/presences.js
@@ -9,54 +9,6 @@ presencesController.controller('presencesCtrl', ['$scope','$rootScope', '$http',
   $scope.goBack=function(){
     window.history.back();
   };
-  /*function demoController(NgTableParams, IssueService) {
-    this.tableParams = new NgTableParams({}, {
-      getData: function(params) {
-        return IssueService.query({
-          page: params.page(),
-          per_page: params.count(),
-
-          state: 'all',
-          username: 'esvit',
-          repo: 'ng-table'
-        }, function(data, headersGetter) {
-          var headers = headersGetter(),
-            pages = headers['link'].split(', '),
-            totalPages = 1;
-
-          // get total pages count
-          angular.forEach(pages, function(page) {
-            var parts = page.split(' rel=');
-            if (parts[1] == '"last"') {
-              totalPages = parseInt(parts[0].match(/page=(\d+)/)[1], 10);
-            }
-            if (totalPages == 1 && parts[1] == '"prev"') { // if last page
-              totalPages = parseInt(parts[0].match(/page=(\d+)/)[1], 10) + 1;
-            }
-          });
-          params.total(totalPages * params.count());
-          return data;
-        }).$promise;
-      }
-    });
-  };
-
-(function() {
-  "use strict";
-
-  angular.module("documentsApp").factory("IssueService", ["$resource", function($resource) {
-    return $resource("https://api.github.com/repos/:username/:repo/issues", {
-      state: "open"
-    }, {
-      query: {
-        method: "GET",
-        isArray: true
-      }
-    });
-  }]);
-});
-}]);*/
-//var data = [{name: "Moroni", age: 50} /*,*/];
   var self = this;
     var json = $.param({personId: $rootScope.userId});
   $http({
@@ -67,42 +19,48 @@ presencesController.controller('presencesCtrl', ['$scope','$rootScope', '$http',
 
   }).then(function mySucces(ris) {
 
-    $rootScope.people=ris.data;
-  $scope.tableParams = new NgTableParams({page: 1, sorting: {
-    Nome: 'asc'
-  },
-  count: 10
 
-}, {
-  //total: $scope.data.length,
-  getData: function(params) {
+  $scope.users=ris.data;
+  $scope.usersTable = new NgTableParams({
 
-    var data = $rootScope.people;
-    data = params.filter() ? $filter('filter')(data, params.filter()) : data;
-    data = params.orderBy() ? $filter('orderBy')(data, params.orderBy()) : data;
-    params.total(data.length);
-    data = data.slice((params.page() - 1) * params.count(), params.page() * params.count());
-    return data;
+  page: 1,
+  count: 5
+  }, {
+  total: $scope.users.length,
+  getData: function ($defer, params) {
+  $scope.data = params.sorting() ? $filter('orderBy')($scope.users, params.orderBy()) : $scope.users;
+  $scope.data = params.filter() ? $filter('filter')($scope.users, params.filter()) : $scope.users;
+  $scope.data = $scope.data.slice((params.page() - 1) * params.count(), params.page() * params.count());
+  $defer.resolve($scope.data);
   }
-
-});
+  });
+  $scope.params.settings().$scope = $scope;
 
   }), function myError(r) {
-      alert("Error "+r);
+      alert("Error: "+r);
   };
 
 
-  /*$scope.people =[{
-           name: "Moroni",
-           age: 44
-       }, {
-           name: "AAAA",
-           age: 23
-       }, {
-           name: "BBBBB",
-           age: 50
-       }];*/
+    /*$scope.tableParams = new NgTableParams({page: 1, sorting: {
+      Nome: 'asc'
+    },
+    count: 10
 
+  }, {
+    //total: $scope.data.length,
+    getData: function(params) {
+
+    alert($rootScope.people);
+      var data = $rootScope.people;
+      alert (data);
+      data = params.filter() ? $filter('filter')(data, params.filter()) : data;
+      data = params.orderBy() ? $filter('orderBy')(data, params.orderBy()) : data;
+      params.total(data.length);
+      data = data.slice((params.page() - 1) * params.count(), params.page() * params.count());
+      return data;
+    }
+
+  });
 
   var sort = function(data, sort) {
     var defer = $q.defer();
@@ -111,7 +69,7 @@ presencesController.controller('presencesCtrl', ['$scope','$rootScope', '$http',
     }
     defer.resolve(data);
     return defer.promise;
-  };
+  };*/
           //  self.tableParams = new NgTableParams({}, { data: data});
 
 

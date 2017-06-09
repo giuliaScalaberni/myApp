045a6eaeae8a1d7b0f72accc799ba7cf28bbaa1a
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-06-09 21:14:52 +0200

    integrating login

--- scripts/controllers/login.js
+++ scripts/controllers/login.js
@@ -9,5 +9,17 @@
  */
     var loginController = angular.module('loginController', []);
 loginController.controller('loginCtrl', function($scope,$rootScope, $http, $route, $location) {
-
+  $scope.onSignIn=function(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  };
+  $scope.signOut=function() {
+     var auth2 = gapi.auth2.getAuthInstance();
+     auth2.signOut().then(function () {
+       console.log('User signed out.');
+     });
+   };
   });

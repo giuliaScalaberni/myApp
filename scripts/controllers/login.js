46bc431efc53b9ea464d1a8cfc6bdede440657c2
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-06-10 11:22:30 +0200

    ok login

--- scripts/controllers/login.js
+++ scripts/controllers/login.js
@@ -9,19 +9,15 @@
  */
     var loginController = angular.module('loginController', []);
 loginController.controller('loginCtrl', function($scope,$rootScope, $http, $route, $location) {
-  /*function onSignIn(googleUser) {
+  function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-  alert('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('ID: ' + profile.getId());
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  };
-  $scope.signOut=function() {
-     var auth2 = gapi.auth2.getAuthInstance();
-     auth2.signOut().then(function () {
-       console.log('User signed out.');
-       $route.reload();
-     });
-   };*/
+    console.log('Email: ' + profile.getEmail());
+   }
+
+
+  window.onSignIn = onSignIn;
 
   });

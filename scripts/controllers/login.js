cc92d3c4cd571df958ce61a763f5a7a6980364ab
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-06-10 12:02:20 +0200

    wip logout

--- scripts/controllers/login.js
+++ scripts/controllers/login.js
@@ -15,9 +15,24 @@ loginController.controller('loginCtrl', function($scope,$rootScope, $http, $rout
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail());
+    $rootScope.mail=profile.getEmail();
+    $rootScope.image=profile.getImageUrl();
+    $scope.login=true;
+  };
+
+   function signOut() {
+     var auth2 = gapi.auth2.getAuthInstance();
+     auth2.signOut().then(function () {
+       console.log('User signed out.');
+       $scope.login=false;
+       $rootScope.mail="";
+       $rootScope.image="";
+     });
+   };
+
+   if ($rootScope.mail!=""){
+     alert($rootScope.mail);
    }
-
-
   window.onSignIn = onSignIn;
 
   });

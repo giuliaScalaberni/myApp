e9b3fdb63f461510aeb2fd5793ef09cb9fae00ff
scalaberni.giulia@gmail.com <scalaberni.giulia@gmail.com>
2017-06-10 12:03:55 +0200

    ok logout

--- scripts/controllers/login.js
+++ scripts/controllers/login.js
@@ -20,7 +20,7 @@ loginController.controller('loginCtrl', function($scope,$rootScope, $http, $rout
     $scope.login=true;
   };
 
-   function signOut() {
+   $scope.signOut=function() {
      var auth2 = gapi.auth2.getAuthInstance();
      auth2.signOut().then(function () {
        console.log('User signed out.');
